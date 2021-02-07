9fd7b63882eea4895b0d7afbee8f39a7448502dd
Cahya Isra <cahya@wvi.co.id>
2021-02-08 00:56:09 +0700

    add file size check

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const argv = process.argv
 const { spawn } = require('child_process')
 const axios = require('axios')
+const { statSync, unlinkSync } = require("fs");
+const { FILE } = require('dns');
 
 function getFileID(url) {
     let FILEID
 
-    if(url.includes('/file/d/')){
+    if (url.includes('/file/d/')) {
         FILEID = url.split('/file/d/')[1].split('/')[0]
     } else {
         FILEID = url.split('id=')[1]
@@ -19,22 +21,22 @@ function getFileID(url) {
     return FILEID
 }
 
-async function getFileName(FILEID, argv = []){
+async function getFileName(FILEID, argv = []) {
     let data
     let FILENAME
-    
+
     argv.forEach((item, idx) => {
-        if(idx > 1 && !item.includes('http')){
+        if (idx > 1 && !item.includes('http')) {
             FILENAME = item
         }
     })
 
-    if(FILENAME){
+    if (FILENAME) {
         return FILENAME
     }
 
     let trueUrl = `https://docs.google.com/uc?id=${FILEID}`
-   
+
     try {
         let res = await axios.get(trueUrl)
         data = res.data
@@ -51,7 +53,7 @@ async function getFileName(FILEID, argv = []){
     } catch (err) {
         console.log(err.message)
     }
-    
+
     try {
         let res = await axios.get(`https://drive.google.com/file/d/${FILEID}/view`)
         data = res.data
@@ -77,8 +79,27 @@ async function handleDownload(argv) {
 
     const FILEID = getFileID(url)
     const FILENAME = await getFileName(FILEID, argv)
+
+    try {
+        const size = getFileSize(FILENAME)
+        if(size < 10){
+            unlinkSync(FILENAME)
+            console.log('FILE SIZE IS UNDER 3MB. RESTARTING DOWNLOAD.')
+        } else {
+            console.log('FILE SIZE IS ABOVE 3MB. CONTINUING...')
+        }
+    } catch (err) {
+        console.log(err.message)
+    }
+
     const command = `wget --continue --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=` + FILEID + `' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*` + "/\\1\\n/p')" + `&id=` + FILEID + `" -O "` + FILENAME + `" && rm -rf /tmp/cookies.txt`
     spawn('sh', ['-c', command], { stdio: 'inherit' });
+}
+
+function getFileSize(path) {
+    const bytes = statSync(path).size
+    const megaBytes = bytes / (1024 * 1024);
+    return megaBytes;
 }
 
 handleDownload(argv)
